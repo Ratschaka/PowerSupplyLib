936e67b6770404258e99ae9ae47742eb08977021
Ratschaka <102407118+Ratschaka@users.noreply.github.com>
2022-03-26 09:10:23 +0100

    Added blank commands to QL355TP

---
+++ Lib/QL355TP.cs
@@ -0,0 +1,59 @@
+class QL355TP:Powersupply
+{
+    public void Reset()
+    {
+        WriteCommand("*RST");
+    }
+    public void Identify()
+    {
+        WriteCommand("*IDN?");
+    }
+    public void SetVoltage()
+    {
+
+    }
+    public void SetCurrent()
+    {
+
+    }
+    public void SetOverVoltage()
+    {
+
+    }
+    public void SetOverCurrent()
+    {
+
+    }
+    public void Ask_SetVoltage()
+    {
+
+    }
+    public void Ask_SetCurrent()
+    {
+
+    }
+    public void AskVoltage()
+    {
+
+    }
+    public void AskCurrent()
+    {
+
+    }
+    public void AskOverCurrent()
+    {
+
+    }
+    public void AskOverVoltage()
+    {
+
+    }
+    public void Range()
+    {
+
+    }
+    public void Output()
+    {
+
+    }
+}
